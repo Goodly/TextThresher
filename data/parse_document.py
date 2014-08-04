f070e13a62e48372d16163a8c5b307ed809b2f9b
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-08-04 12:33:21 -0400

    Move files containing errors to an appropriate error directory. Detect duplicate/useless articles.

--- data/parse_document.py
+++ data/parse_document.py
@@ -3,16 +3,22 @@ import os
 import re
 from datetime import date
 
-SUCCESS_FOLDER = "DecidingForceArticles"
-ARTICLE_FOLDER = "DecidingForceErrors"
-ERROR_FOLDER = "DecidingForceErrors_v2"
+#SUCCESS_FOLDER = "DecidingForceArticles"
+ARTICLE_FOLDER = "DecidingForceArticles"
+FILENAME_ERROR_FOLDER = "DecidingForceErrors/filename"
+HEADER_ERROR_FOLDER = "DecidingForceErrors/header"
+TEXT_ERROR_FOLDER = "DecidingForceErrors/text"
+DUPLICATES_ERROR_FOLDER = "DecidingForceErrors/duplicates"
 #ARTICLE_FOLDER = "DecidingForceArticles_Sample/"
+
+# List of known duplicates
+DUPLICATES_FILE = "duplicates.txt"
 
 # Format of the article filenames.
 # Example: 1000BethlehemPA-LehighValleyLive-02.txt
 FILENAME_RE = (r'^(?P<article_id>\d+)' # article ID
                '-?' # optional hyphen
-               '(?P<city>[\w\ -]+)' # city name
+               '(?P<city>[\w\ \.-]+)' # city name
                ',? ?' # optional space and comma
                '(?P<state>[A-Z][A-Z])?' # optional state name
                '-' # hyphen
@@ -49,12 +55,28 @@ ANNO_RE = r'(<!!>)?\s*(?P<annotator>[A-Z]+)\s*(</>)?'
 # Format of the version. Example: v23
 VERSION_RE = r'v(?P<version>\d+)'
 
+class ArticleParseError(Exception):
+    FILENAME_ERROR = 0
+    HEADER_ERROR = 1
+    TEXT_ERROR = 2
+    DUPLICATE_ERROR = 3
+
+    def __init__(self, message, error_type):
+        super(ArticleParseError, self).__init__(message)
+        self.error_type = error_type
+
+
 def parse_document(path):
     with open(path, 'r') as f:
         raw_text = f.read()
 
     # extract info from the file name
     article_id, city, state, periodical, periodical_code = parse_filename(path)
+
+    # don't parse known duplicates
+    if article_id in parse_duplicates():
+        raise ArticleParseError("Article %s is known duplicate!" % article_id,
+                                ArticleParseError.DUPLICATE_ERROR)
 
     # extract info from the header and remove it from the text
     (clean_text, date_published, annotators, version) = parse_header(raw_text)
@@ -68,9 +90,20 @@ def parse_document(path):
             tuas[tua_type] = {}
         if tua_id not in tuas[tua_type]:
             tuas[tua_type][tua_id] = []
-        assert (clean_text[tua_span[0]:tua_span[1]].strip().lower()
-                == tua_body.strip().lower())
+
+        if (clean_text[tua_span[0]:tua_span[1]].strip().lower()
+            != tua_body.strip().lower()):
+            raise ArticleParseError(
+                "Reconstructed clean text didn't match TUA body!",
+                ArticleParseError.TEXT_ERROR)
+
         tuas[tua_type][tua_id].append(tua_span)
+
+    # If the only tua_type is 'Useless', this document is likely a duplicate.
+    if len(tuas.keys()) == 1 and 'Useless' in tuas.keys():
+        print "Possibly useless:", article_id
+        raise ArticleParseError("Only found Useless tuas!",
+                                ArticleParseError.DUPLICATE_ERROR)
 
     # print out our data.
     # TODO: store this somewhere.
@@ -117,9 +150,13 @@ def parse_header(raw_text):
         elif header_rownum == 3:
             annotators, version = parse_annotator_line(line)
             if not annotators and not version:
-                raise ValueError('Unexpected header line 3: ' + line)
+                raise ArticleParseError("Unexpected header line 3: " + line,
+                                        ArticleParseError.HEADER_ERROR)
             break
 
+    if header_rownum != 3:
+        raise ArticleParseError("Not all header lines found!",
+                                ArticleParseError.HEADER_ERROR)
     headerless_text = '\n'.join(lines[i+1:]).strip()
     return (headerless_text, date_published, annotators, version)
 
@@ -130,9 +167,15 @@ def parse_date_line(raw_text):
     date_published = None
     if match:
         month, day, year = match.group('month', 'day', 'year')
-        date_published = date(2000 + int(year), int(month), int(day))
-    elif 'Undated' not in raw_text:
-        raise ValueError('Unexpected header line 2: ' + line)
+        try:
+            date_published = date(2000 + int(year), int(month), int(day))
+        except ValueError:
+            raise ArticleParseError("Parsed an Invalid Date: " + raw_text,
+                                    ArticleParseError.HEADER_ERROR)
+    elif ('Undated' not in raw_text and 'undated' not in raw_text
+          and '???' not in raw_text):
+        raise ArticleParseError('Unexpected header line 2: ' + raw_text,
+                                ArticleParseError.HEADER_ERROR)
 
     return date_published
 
@@ -167,7 +210,7 @@ def parse_and_clean_tuas(raw_text):
         has_brackets, clean_text, new_tuas = extract_bracket_text(match, raw_text)
         if has_brackets:
             raw_text = clean_text
-            for new_tua_body, new_tua_index in new_tuas:
+            for new_tua_body, new_tua_index, num_matches in new_tuas:
                 if new_tua_index == 0: # forward reference--we need to wait.
                     tuas_to_finalize.append((t_id, t_type, new_tua_body,
                                              new_tua_index))
@@ -178,7 +221,7 @@ def parse_and_clean_tuas(raw_text):
                     tuas.append((t_id, t_type, new_tua_body, t_span))
         else: # No brackets to remove, just parse and clean the TUA.
             t_start = match.start('tua_body')
-            t_index = find_match_index(re.escape(t_body), t_start, raw_text)
+            t_index, _ = find_match_index(re.escape(t_body), t_start, raw_text)
             raw_text = merge_with_spacing((raw_text[:match.start()],
                                           t_body,
                                            raw_text[match.end():]))
@@ -188,9 +231,17 @@ def parse_and_clean_tuas(raw_text):
         match = re.search(TUA_RE, raw_text)
 
     for t_id, t_type, t_body, t_index in tuas_to_finalize:
-        t_span = get_match_span_by_index(re.escape(t_body.strip()), t_index,
-                                         raw_text)
-        tuas.append((t_id, t_type, t_body, t_span))
+        try:
+            t_span = get_match_span_by_index(re.escape(t_body.strip()), t_index,
+                                             raw_text)
+            tuas.append((t_id, t_type, t_body, t_span))
+
+        # This wasn't a forward reference--just normally occuring brackets.
+        # So don't create the TUA.
+        # WARNING!! This eliminates the bracket contents from the article!!
+        # Option: put the contents back? (they're in t_body)
+        except ArticleParseError:
+            pass
 
     return (tuas, raw_text)
 
@@ -199,11 +250,12 @@ def find_match_index(regex, match_start, source_text):
     # match that begins at offset match_start.
     # Returns None if there is no such match
     match_index = None
+    num_matches = 0
     for match_num, match in enumerate(re.finditer(regex, source_text, re.I)):
+        num_matches += 1
         if match.start() == match_start:
             match_index = match_num
-            break
-    return match_index
+    return (match_index, num_matches)
 
 def get_match_span_by_index(regex, match_index, source_text):
     # Return the index into source_text where regex matches the text for the
@@ -211,7 +263,8 @@ def get_match_span_by_index(regex, match_index, source_text):
     for match_num, match in enumerate(re.finditer(regex, source_text, re.I)):
         if match_num == match_index:
             return match.span()
-    raise ValueError("Match doesn't occur correct number of times")
+    raise ArticleParseError("Match doesn't occur correct number of times",
+                            ArticleParseError.TEXT_ERROR)
 
 def extract_bracket_text(match, source_text):
     # brackets might be anywhere starting from the character before the match to
@@ -240,14 +293,14 @@ def extract_bracket_text(match, source_text):
         bracket_text = bracket.group('text')
         if bracket_text == 'DT': # special case--this is never a true bracket.
             continue
-        bracket_text_index = find_match_index(
+        bracket_text_index, num_matches = find_match_index(
             re.escape(bracket_text),
             m_start - 1 + bracket.start('text'),
             source_text)
         # at least one occurrence before the bracket--back reference.
         if bracket_text_index != 0:
             bracket_text_index -= 1
-        tuas_to_create.append((bracket_text, bracket_text_index))
+        tuas_to_create.append((bracket_text, bracket_text_index, num_matches))
 
         # Track the text we need to remove
         # The bracket match is relative to the TUA match, so ajdust the offset.
@@ -305,18 +358,33 @@ def parse_filename(filename):
         return match.group('article_id', 'city', 'state', 'periodical',
                            'periodical_code')
     else:
-        raise ValueError('bad file name: ' + raw_name)
+        raise ArticleParseError('Bad File Name: ' + raw_name,
+                                ArticleParseError.FILENAME_ERROR)
 
-def parse_documents(directory_path, error_directory_path):
+def parse_duplicates():
+    with open(DUPLICATES_FILE, 'r') as dup_f:
+        return dup_f.readlines()
+
+def parse_documents(directory_path, error_directory_paths):
     for file_path in os.listdir(directory_path):
-        print "PROCCESING FILE:", file_path, "..."
         full_path = os.path.join(directory_path, file_path)
+        if '.gitignore' in file_path or os.path.isdir(full_path):
+            continue
+        print "PROCCESING FILE:", file_path, "..."
+
         try:
             parse_document(full_path)
-        except Exception:
-            raise
-            os.rename(full_path, os.path.join(error_directory_path, file_path))
+        except ArticleParseError as e:
+            new_path = os.path.join(error_directory_paths[e.error_type],
+                                    file_path)
+            os.rename(full_path, new_path)
             print "ERROR!"
 
 if __name__ == '__main__':
-    parse_documents(ARTICLE_FOLDER, ERROR_FOLDER)
+    error_dirs = {
+        ArticleParseError.FILENAME_ERROR : FILENAME_ERROR_FOLDER,
+        ArticleParseError.HEADER_ERROR : HEADER_ERROR_FOLDER,
+        ArticleParseError.TEXT_ERROR : TEXT_ERROR_FOLDER,
+        ArticleParseError.DUPLICATE_ERROR: DUPLICATES_ERROR_FOLDER,
+    }
+    parse_documents(ARTICLE_FOLDER, error_dirs)
