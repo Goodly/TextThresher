542fd5fee12ea9181008b4b0f2c6a6ede8edf3ea
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-09-12 16:51:49 -0700

    Set up staticfiles for Django correctly.

--- thresher_backend/settings.py
+++ thresher_backend/settings.py
@@ -91,5 +91,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
