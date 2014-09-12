6086f928cc7506a0034490df3b833b72e77a5d10
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-09-12 16:07:06 -0700

    Deleted all the old frontend stuff.

--- js/decidingForceAnnotationTool.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    // Setup click-to-hide on the question panels
-    $('[data-toggle=divpanel]').click(function (clickEvent) {
-        $($(clickEvent.target).data("target")).toggleClass("hidden");
-    });
-
-    // Start the colorpicker.
-    $('#highlightColorpicker').colorpicker();
-});
