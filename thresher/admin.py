b1a451f3407d084eddd572235e516371d20caeb5
Fady Shoukry <fady.shoukry@mail.utoronto.ca>
2014-10-25 15:04:54 -0700

    Added the new models to the admin page

--- thresher/admin.py
+++ thresher/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from thresher.models import Article, AnalysisType, TUA
+from thresher.models import *
 
 admin.site.register(Article)
 admin.site.register(AnalysisType)
 admin.site.register(TUA)
+admin.site.register(Topic)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(MCHighlightGroup)
+admin.site.register(CLHighlightGroup)
+admin.site.register(TBHighlightGroup)
+admin.site.register(DTHighlightGroup)
+
