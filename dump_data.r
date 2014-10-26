050a178a0025f33d316d97d48cc27761ecdb3079
Daniel Haas <dhaas@eecs.berkeley.edu>
2014-10-25 19:43:14 -0700

    Added R script for extracting TUA data.

---
+++ dump_data.r
@@ -0,0 +1,6 @@
+library(RPostgreSQL)
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="thresher")
+rs <- dbSendQuery(con, 'select T.id as tua_id, T.offsets as tua_offsets, A.text as article_text, A.city_published as city, A.date_published as date, A.article_id, A.periodical_code, A.annotators as annotators, TT.name as tua_type from thresher_tua T, thresher_article A, thresher_analysistype TT where T.article_id = A.article_id and T.analysis_type_id = TT.id;')
+data <- fetch(rs, n=-1)
+write.csv(data, file = "tua_data.csv", fileEncoding = "UTF-8")
