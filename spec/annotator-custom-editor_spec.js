811eaaf0c86fa7f3989945c54c2cbd9d32d59c5c
Tyler Burton <btburton42@gmail.com>
2015-04-19 16:20:12 -0700

    Ports project to ES6. Adds LiveReload

---
+++ spec/annotator-custom-editor_spec.js
@@ -0,0 +1,9 @@
+describe('Annotator Custom Editor ', function() {
+  return describe('Give it some context', function() {
+    return describe('maybe a bit more context here', function() {
+      return it('should run here few assertions', function() {
+        return console.log('change me');
+      });
+    });
+  });
+});
