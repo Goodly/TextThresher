55975febae759b3aa28455dfd1d243ac6e4b9bd9
Tyler Burton <btburton42@gmail.com>
2015-04-26 12:53:01 -0700

    Loading new 2.0 Annotator with default UI   • Retrieve JSON payload from TT Backend   • Setup Promise function for Intercepting Annotator UI

--- src/annotator-custom-editor.js
+++ src/annotator-custom-editor.js
@@ -1,27 +1,54 @@
-Annotator.Plugin.CustomEditor = class CustomEditor extends Annotator.Plugin{
-  pluginInit(){
-    this.annotator
-      .subscribe('beforeAnnotationCreated', annotation => {
-        this.customSetup(annotation);
+function annotatorCustomEditor(options){
+  return {
+    start: function() {
+      this.options = options;
+      this.getDataPayload();
+    },
+
+    // ajax get to retrieve data
+    getDataPayload: function(){
+      $.get(this.options.endpoint, (data) => {
+        this.configureData(data);
       })
-      .subscribe('annotationCreated', annotation => {
-        this.newCustomMethod(annotation);
+    },
+
+    // method to model our raw data payload
+    // @param value [object] API payload returned via AJAX
+    //
+    configureData: function(value){
+      // TODO: establish how we want to configure the data
+      this.data = {
+        next: value.next,
+        previous: value.previous,
+        glossary: value.results[0].analysis_type.glossary
+      }
+    },
+
+    // hooks into Annotator lifecycle events
+    //
+    // TODOS:
+    //  • how to create our own UI
+    //  • how best to store our answers to interact with other plugins
+    //  • how to pause the annotator lifecycle?
+    beforeAnnotationCreated: function(annotation){
+      return this.promiseHelper(annotation)
+        .then((value) => {
+          this.setUpCustomEditorPanel(value);
+        });
+    },
+
+    setUpCustomEditorPanel: function(annotation) {
+      console.log(annotation);
+    },
+
+    // helper promise function for intercepting Annotator UI
+    //
+    promiseHelper: function(annotation) {
+      return new Promise((resolve, reject) => {
+        let value = annotation
+        resolve(value);
       })
-      .subscribe('annotationUpdated', annotation => {
-        this.newCustomMethod(annotation);
-      })
-      .subscribe('annotationDeleted', annotation => {
-        this.newCustomMethod(annotation);
-      })
+    }
 
   }
-
-  customSetup(annotation){
-    console.log(annotation);
-  }
-
-  newCustomMethod(annotation){
-    console.log(annotation);
-  }
-
 }
