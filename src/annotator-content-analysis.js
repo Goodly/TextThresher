6041e5bd6019690d37284b68e6b9b25e8a97e9c9
Tyler Burton <btburton42@gmail.com>
2015-06-06 17:13:06 -0700

    Appends TUA data to DOM • Pulls in valid TUA text from endpoint. • Adds static JSON to minimize network dependency • Adds basic style sheet and preprocessor compile step • Adds Handlebars support

--- src/annotator-content-analysis.js
+++ src/annotator-content-analysis.js
@@ -1,54 +1,143 @@
-function annotatorCustomEditor(options){
+function annotatorContentAnalysis(options){
   return {
-    start: function() {
+    start() {
       this.options = options;
-      this.getDataPayload();
+      this._elements();
+      this.getTemplates();
+      this.getData();
     },
 
-    // ajax get to retrieve data
-    getDataPayload: function(){
-      $.get(this.options.endpoint, (data) => {
-        this.configureData(data);
-      })
-    },
-
-    // method to model our raw data payload
-    // @param value [object] API payload returned via AJAX
-    //
-    configureData: function(value){
-      // TODO: establish how we want to configure the data
-      this.data = {
-        next: value.next,
-        previous: value.previous,
-        glossary: value.results[0].analysis_type.glossary
+    _elements() {
+      this.survey = {
+        answers: [],
+        topics: [],
+        questions: []
       }
     },
 
-    // hooks into Annotator lifecycle events
+    getTemplates() {
+      this.templates = {}
+      $.each(this.options.templates, (key, value, index) => {
+        let template = this.getUrl(value);
+        template.then((res) => {
+          this.templates[key] = res;
+        })
+      });
+    },
+
+    // GET data via AJAX or pass along data object
     //
-    // TODOS:
-    //  • how to create our own UI
-    //  • how best to store our answers to interact with other plugins
-    //  • how to pause the annotator lifecycle?
-    beforeAnnotationCreated: function(annotation){
-      return this.promiseHelper(annotation)
-        .then((value) => {
-          this.setUpCustomEditorPanel(value);
+    getData(){
+      if (!!this.options.data) {
+        this.setData(this.options.data);
+      } else {
+        let data = this.getUrl(this.options.dataUrl);
+        data.then((res) => {
+          this.setData(res)
+        });
+      }
+    },
+
+    // shortcut for AJAX get
+    getUrl(url) {
+      return $.get(url, (data) => {
+        return data;
+      });
+    },
+
+    // #TODO: make this more universal
+    //
+    // method to cast our data payload into module's context
+    // @param value [object] API payload returned via AJAX
+    //
+    setData(value){
+      this.data = {
+        next: value.next,
+        previous: value.previous,
+        analysis_type: value.results[0].analysis_type
+      }
+    },
+
+    // returns a promise that isn't resolved until our survey is submitted
+    //
+    createSurvey(annotation) {
+      return new Promise((resolve, reject) => {
+        this.data.analysis_type.topics.forEach((element) => {
+          this.getTopics(element)
+        });
+        let data = {
+          thisTopics: this.survey.topics.join('')
+        }
+
+        let formTemplate = Handlebars.compile(this.templates.form)
+
+        $('body').append(formTemplate(data))
+
+        $('.submit').on('click', (e) => {
+          console.log($('form').serialize())
+          resolve(annotation)
+          return false
+
+        })
+
+      })
+    },
+
+    getTopics(element) {
+      element.questions.forEach((element) => {
+        this.getQuestions(element)
+      });
+      let data = {
+        element,
+        thisQuestions: this.survey.questions.join('')
+      }
+      this.survey.questions = []
+      let topicTemplate = Handlebars.compile(this.templates.topic);
+
+      this.survey.topics.push(topicTemplate(data))
+    },
+
+    getQuestions(element) {
+      this.parentId = element.id
+      element.answers.forEach((element) => {
+        this.getAnswers(element)
+      });
+      let data = {
+        element,
+        thisAnswers: this.survey.answers.join('')
+      }
+      this.survey.answers = []
+      let questionTemplate = Handlebars.compile(this.templates.question);
+      this.survey.questions.push(questionTemplate(data))
+    },
+
+    getAnswers(element) {
+      let data = {
+        element,
+        parentId: this.parentId
+      }
+      let answerTemplate = Handlebars.compile(this.templates.answer);
+
+      this.survey.answers.push(answerTemplate(data))
+    },
+
+    // this function runs once the promise in this.beforeAnnotationCreated() has been resolved
+    postResults(annotation) {
+      console.log('postResults');
+    },
+
+    // ANNOTATOR LIFECYCLE EVENTS
+    //
+    beforeAnnotationCreated(annotation){
+      return this.createSurvey(annotation)
+        .then((annotation) => {
+          return this.postResults(annotation);
         });
     },
 
-    setUpCustomEditorPanel: function(annotation) {
-      console.log(annotation);
-    },
-
-    // helper promise function for intercepting Annotator UI
-    //
-    promiseHelper: function(annotation) {
-      return new Promise((resolve, reject) => {
-        let value = annotation
-        resolve(value);
-      })
-    }
+    annotationCreated(annotation) {
+      return annotation
+   }
 
   }
 }
