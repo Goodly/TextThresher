e49630ee7f824758332b6e96278ac5649b1d7d76
Tyler Burton <btburton42@gmail.com>
2016-01-29 16:58:42 -0800

    patch updates  - deleting unneeded gulp file and annotator

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -2,6 +2,7 @@
 
 require('babel/register');
 
+var colors = require('colors');
 var debug = require('debug')('dev');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
@@ -14,3 +15,6 @@ var devServer = new WebpackDevServer(compiler, config.server.options);
 devServer.listen(config.server.port, 'localhost', function () {
   debug('webpack-dev-server listening on port %s', config.server.port);
 });
+
+console.log('server available at:'.underline.yellow);
+console.log('http://localhost:3001/app/#/tua/0/topic/0/question/0'.underline.yellow);
