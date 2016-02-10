ae8a3c34e6d716cc40473dba05dc65374c572672
Arjun Mehta <arjunmehta94@berkeley.edu>
2016-02-10 13:48:19 -0800

    squash commits for PR adds idClicked state to keep track of which answer clicked adds classnames module for easy class naming, adds isSelected and id props for selecting correct answer adds isSelected and id props for propagation to QuizAnswer adds answer and highlight answer style adds classnames to dependencies displays random text when answer clicked adds idClicked state to keep track of which answer clicked adds classnames module for easy class naming, adds isSelected and id props for selecting correct answer adds isSelected and id props for propagation to QuizAnswer displays random text when answer clicked fixes minor newline and quotation error highlights checkbox text, and for radio buttons highlights in color of selected radio option refactors QuizQuestion to be self contained, fixes cross-cutting, works with >2 questions remove stray console.log remove unused variables, cleaned up removes unnecessary prop and import removes unncessary imports Changes QuizAnswer implementation from JQuery to States Removes unncessary CSS Changes palette to default function instead of class removes unnecessary id attribute ;

--- app/components/quiz/QuizQuestion.js
+++ app/components/quiz/QuizQuestion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import QuizAnswer from 'components/quiz/QuizAnswer.js';
 import QuizContext from 'components/quiz/QuizContext.js';
+import randomColor from 'utils/palette';
 
 export default React.createClass({
   displayName: 'QuizQuestion',
@@ -10,20 +11,52 @@ export default React.createClass({
     onUpdate: React.PropTypes.func.isRequired
   },
 
-  onChange: function(e) {
-    // just propagate up to parent
-    this.props.onUpdate(e);
+  getInitialState: function() {
+    var isSelected = new Array(this.props.question.answers.length).fill(false);
+    var randomColors = this.props.question.type === 'checkbox' ?
+                       randomColor(this.props.question.answers.length) :
+                       randomColor(1);
+    return {colors: randomColors, isSelectedFlags: isSelected}
+  },
+
+  answerClicked: function(id, selected) {
+    // from child, which id of answer was clicked.
+    var isSelectedFlags = this.state.isSelectedFlags;
+    if (this.props.question.type === 'checkbox') {
+      // if it is a checkbox, it should update ONLY the required ID flag, leave others
+      isSelectedFlags[parseInt(id)] = selected;
+      this.setState({isSelectedFlags: isSelectedFlags});
+    }
+    else {
+      // if it is a radio, it should set ALL others to false, only this id flag to true
+      isSelectedFlags = Array(isSelectedFlags.length).fill(false);
+      isSelectedFlags[parseInt(id)] = selected;
+      this.setState({isSelectedFlags: isSelectedFlags});
+    }
+    this.props.onUpdate(this.props.question.id, isSelectedFlags.includes(true));
   },
 
   render() {
     return (
       <div className='quiz__question'>
-        <QuizContext context={this.props.question.context} />
+        <QuizContext context={this.props.question.context}/>
         <br/>
         <p>{this.props.question.text}</p>
-        <form onChange={this.onChange}>
-          {this.props.question.answers.map((answer) => {
-            return <QuizAnswer key={answer.text} answer={answer}/>;
+        <form>
+          {this.props.question.answers.map((answer, i) => {
+            var isSelected = this.state.isSelectedFlags[i]
+            var color = this.props.question.type === 'checkbox' ?
+                                           this.state.colors[i] :
+                                           this.state.colors[0] ;
+            return (
+              <QuizAnswer key={answer.text}
+                          answer={answer}
+                          isSelected={isSelected}
+                          id={i}
+                          colors={color}
+                          answerClicked={this.answerClicked}
+                          type={this.props.question.type}/>
+            );
           })}
         </form>
       </div>
---
+++ app/utils/palette.js
@@ -0,0 +1,12 @@
+var colorOptions = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00',
+										'#ffff33','#a65628','#f781bf','#999999','#8dd3c7',
+										'#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462',
+										'#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5',
+										'#ffed6f'];
+export default function getUniqueRandomColor(count) {
+  var results = new Set([]);
+  while (results.size !== count) {
+    results.add(colorOptions[Math.floor(Math.random() * colorOptions.length)]);
+  }
+  return Array.from(results);
+};
