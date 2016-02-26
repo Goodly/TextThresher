f6b9c07ee67e7cd48af6d0531d37a9e72545deaf
Piyush Patil <piyush.patil210@gmail.com>
2016-02-25 18:47:53 -0800

    Implemented User and Client classes

--- thresher/admin.py
+++ thresher/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from thresher.models import *
+from django.contrib.auth.admin import UserAdmin
 
+class UserProfileInLine(admin.StackedInline):
+	model = UserProfile
+	verbose_name_plural = 'user'
+
+class NewUserAdmin(UserAdmin):
+	inlines = [
+		UserProfileInLine,	
+	]
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, NewUserAdmin)
+
+admin.site.register(Client)
 admin.site.register(Article)
 admin.site.register(AnalysisType)
 admin.site.register(TUA)
