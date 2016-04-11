d41da123403b4c0cd91e35b0bff395c9ad9453e6
Kevin Lee <kevinmlee.x@gmail.com>
2016-04-10 23:37:45 -0700

    support redux dev tools chrome extension

--- app/store/appStore.js
+++ app/store/appStore.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, compose(
+    window.devToolsExtension ? window.devToolsExtension() : undefined
+  ));
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
