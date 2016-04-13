6b586c0d4528a55788efba7ef89e67460ed16834
Piyush Patil <piyush.patil210@gmail.com>
2016-04-12 18:15:57 -0700

    Tried moving initialization of target inside constructor

--- thresher/serializers.py
+++ thresher/serializers.py
@@ -259,10 +259,9 @@ class OffsetField(serializers.Field):
 
 class TUASerializer(serializers.Serializer):
     # W3 Annotation Data Model properties
-    # def __init__(self, offsets):
-    #     self.offsets = offsets
-
-    target = OffsetField(TUA.offsets) # Here I'm assuming that TUASerializer has access to the 'offsets' field of TUA
+    def __init__(self, offsets):
+        self.offsets = offsets
+        target = OffsetField(self.offsets)
 
     # Keep TUA metadata
     analysis_type = AnalysisTypeSerializer()
