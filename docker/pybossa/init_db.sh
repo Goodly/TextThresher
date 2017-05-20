8225f8e1f5a03388823b06fb123733c28bc88a09
Norman Gilmore <git@teamforecast.com>
2017-05-20 09:38:10 -0700

    Extracted Pybossa docker build to external repo. Use image built by that repo. (#114)

--- docker/pybossa/init_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-sudo -u pybossa sh /ansible_build/init_db2.sh
--- docker/pybossa/init_db2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/pybossa/repo
-. /home/pybossa/env/bin/activate
-python cli.py db_create
