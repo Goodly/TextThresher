091eb9af5a6a267d515bea1c148f9a5dbf9d18f1
Norman Gilmore <git@teamforecast.com>
2017-05-26 13:31:25 -0700

    Resolve host volume writes (#119)
    * Disable .pyc file generation - they can't be written to mounted host volume.
    
    * Run container with root in order to update model diagrams on host volume.

--- docs/generate-models-diagram.sh
+++ docs/generate-models-diagram.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# docker-compose exec thresher_api ./docs/generate-models-diagram.sh
+# docker-compose exec --user root thresher_api ./docs/generate-models-diagram.sh
 ./docs/install-graphviz.sh
 export DJANGO_SETTINGS_MODULE=thresher_backend.graphviz_settings
 python manage.py graph_models --pygraphviz -o docs/thresher_models.png thresher
---
+++ docs/update-diagrams.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose exec --user root thresher_api ./docs/generate-models-diagram.sh
