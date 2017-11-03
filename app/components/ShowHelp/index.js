75ebe81802d9bc8fa60f1d45a2906b89b147638d
Flora Xue <xuezhenruo@berkeley.edu>
2017-11-03 03:56:30 -0700

    Added video link parsing to show help

--- app/components/ShowHelp/index.js
+++ app/components/ShowHelp/index.js
@@ -17,14 +17,40 @@ class ShowHelp extends Component {
     super(props);
   }
 
-  static propTypes = { 
-    closeHelp: React.PropTypes.func.isRequired 
+  static propTypes = {
+    closeHelp: React.PropTypes.func.isRequired
   }
 
   render() {
     let name = this.props.name;
     let description = this.props.description;
     let long_description = this.props.long_description;
+    // Find the help video URL
+    var url = '';
+    if (long_description.substring(0, 10) === "++++++++++") {
+      // Find the ending syntax
+      var i = 10;
+      var found = false;
+      while (i < long_description.length) {
+        if (long_description.substring(i, i+1) === "+") {
+          if (long_description.substring(i, i+10) === "++++++++++") {
+            found = true;
+            break;
+          }
+        }
+        i += 1;
+      }
+      if (found == true) {
+        url = long_description.substring(10, i).trim();
+        if (url.substring(0, 4) != 'http') {
+          url = 'http://' + url;
+        }
+        long_description = long_description.substring(i+10, long_description.length);
+      }
+    }
+
+    console.log(url);
+
     let help_content = {
       __html: markdown.toHTML(long_description)
     };
@@ -40,6 +66,9 @@ class ShowHelp extends Component {
         <h4 className="project-description">
           {description}
         </h4>
+        <div className="video-link">
+          <a href={url}>Video Tutorial</a>
+        </div>
         <div className="help-contents" dangerouslySetInnerHTML={help_content}>
         </div>
         <button onClick={ () => { this.props.closeHelp(); } } className='close-help-bottom'>
