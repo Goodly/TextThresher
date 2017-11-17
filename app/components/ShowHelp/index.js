a63dd2a91b79c2ebd95a4047b85f3a230779372c
Flora Xue <xuezhenruo@berkeley.edu>
2017-11-17 15:19:53 -0800

    Update the parsing to support multiple links, and use better syntax to denote.

--- app/components/ShowHelp/index.js
+++ app/components/ShowHelp/index.js
@@ -21,35 +21,56 @@ class ShowHelp extends Component {
     closeHelp: React.PropTypes.func.isRequired
   }
 
+  parseVideoUrl(long_description) {
+    var links = [];
+    var lines = long_description.split(/\r\n?|\n/)
+    var found = false;
+    var links_start = long_description.length;
+    var ch_count = 0;
+    for (var i = 0; i < lines.length; i++) {
+      if (found && lines[i]) {
+        var start = lines[i].indexOf("|") + 1;
+        var text = lines[i].substring(0, start - 1).trim();
+        var url = lines[i].substring(start, lines[i].length).trim();
+        if (url.substring(0, 4) != 'http') {
+          url = 'http://' + url;
+        }
+        links.push([text, url]);
+      }
+      if (lines[i] === "+++options+++") {
+        found = true;
+        links_start = ch_count;
+      }
+      ch_count = ch_count + lines[i].length + 1;
+    }
+    return [links, links_start];
+  }
+
   render() {
     let name = this.props.name;
     let description = this.props.description;
     let long_description = this.props.long_description;
     // Find the help video URL
-    var url = '';
-    if (long_description.substring(0, 10) === "++++++++++") {
-      // Find the ending syntax
-      var i = 10;
-      var found = false;
-      while (i < long_description.length) {
-        if (long_description.substring(i, i+1) === "+") {
-          if (long_description.substring(i, i+10) === "++++++++++") {
-            found = true;
-            break;
-          }
-        }
-        i += 1;
-      }
-      if (found == true) {
-        url = long_description.substring(10, i).trim();
-        if (url.substring(0, 4) != 'http') {
-          url = 'http://' + url;
-        }
-        long_description = long_description.substring(i+10, long_description.length);
-      }
-    }
+    var parsed = this.parseVideoUrl(long_description);
+    var links = parsed[0];
+    var links_start = parsed[1];
+    long_description = long_description.substring(0, links_start + 1);
 
-    console.log(url);
+    console.log(links);
+    console.log(links_start);
+    console.log(long_description);
+
+    var links_html;
+    var markup;
+    if (links) {
+      links_html = links.map((link, index) =>
+                    <div key={index}>
+                      <a href={link[1]}>{link[0]}</a>
+                    </div>);
+      markup = <div>{links_html}</div>;
+    } else {
+      markup = <div></div>;
+    }
 
     let help_content = {
       __html: markdown.toHTML(long_description)
@@ -66,9 +87,7 @@ class ShowHelp extends Component {
         <h4 className="project-description">
           {description}
         </h4>
-        <div className="video-link">
-          <a href={url}>Video Tutorial</a>
-        </div>
+        {markup}
         <div className="help-contents" dangerouslySetInnerHTML={help_content}>
         </div>
         <button onClick={ () => { this.props.closeHelp(); } } className='close-help-bottom'>
